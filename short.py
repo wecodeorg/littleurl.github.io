44c9c8fabc4ed494b2b42c11d7bc1f3381fff3a3
Ankit Warbhe <ankitwarbheofficial@gmail.com>
2020-08-15 12:29:54 +0530

    Create short.py

---
+++ short.py
@@ -0,0 +1,28 @@
+import json
+import os
+import shutil
+
+
+def main():
+    """Main Function"""
+    html = "<html><script>window.location.replace('{url}');</script></html>"
+
+    with open('urls.json') as f:
+        links = json.load(f)
+
+    shutil.rmtree('public', ignore_errors=True)
+    os.mkdir('public')
+
+    with open('public/CNAME', 'w') as f:
+        f.write('ankitwarbhe.me')
+
+    for link in links:
+        html_document = html.format(url=link['url'])
+        file_path = f"public/{link['name']}.html"
+
+        with open(file_path, 'w') as f:
+            f.write(html_document)
+
+
+if __name__ == "__main__":
+    main()
